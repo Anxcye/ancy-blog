8be4863a672be760f0467ed43df9fec635781e66
Anxcye <axyyu@outlook.com>
2024-10-16 20:56:11 +0800

    build: add maven build plugin

--- ancy-backend/ancy-common/src/test/java/com/anxcye/TestJwt.java
+++ ancy-backend/ancy-common/src/test/java/com/anxcye/TestJwt.java
@@ -1,36 +1,7 @@
 package com.anxcye;
 
-import com.anxcye.utils.JwtUtil;
-import io.jsonwebtoken.Claims;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class TestJwt {
-
-    @Test
-    public void testJwt() {
-        String jwt = JwtUtil.createJWT("admin");
-        System.out.println(jwt);
-    }
-
-//    @Test
-//    public void testJwt2() {
-//        Claims claim = null;
-//        try {
-//            claim = JwtUtil.parseJWT("eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqb2UifQ.1lnR-gjeq1S5G5PklO4mVDo7hcd5ovQ-BB-MT68QgP4");
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        System.out.println(claim);
-//    }
-
-    @Test
-    public void createAndParse() throws Exception {
-        String  jwt = JwtUtil.createJWT("admin");
-        Claims claims = JwtUtil.parseJWT(jwt);
-        System.out.println(claims);
-
-    }
-
 }
